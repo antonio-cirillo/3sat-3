1149c76acacaeee9230cfe4e229915d8b12ac1ac
Antonio Cirillo <dev.cirillo@gmail.com>
2023-01-17 21:16:31 +0100

    added input request

--- 3sat3.py
+++ 3sat3.py
@@ -7,6 +7,7 @@ from scipy.sparse import csr_matrix
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import argparse
 import re
 
 import matplotlib
@@ -15,18 +16,28 @@ matplotlib.use('TkAgg')
 PHI_REGEX = re.compile('^(\(-?\w+ v -?\w+ v -?\w+\))( \^ \((-?\w+ v -?\w+ v -?\w+)\))*$')
 
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 def generate_random_formula():
-    # variables = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    variables = ['A', 'B', 'C', 'D']
-    n_occurrences = [0, 0, 0, 0, 0, 0, 0, 0]
+    _variables = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    n_occurrences = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     _phi = ''
 
     while 1:
         # get the literals appeared less than 3 times
         temp = []
-        for j in range(len(variables)):
+        for j in range(len(_variables)):
             if n_occurrences[j] < 3:
-                temp.append(variables[j])
+                temp.append(_variables[j])
 
         if len(temp) == 0:
             break
@@ -36,10 +47,10 @@ def generate_random_formula():
             x1, x2, x3 = np.random.choice(temp, 3, replace=False)
         # if there are not at least 3 literals, add 2 more literals to the formula
         except ValueError:
-            max_variables = max(variables)
+            max_variables = max(_variables)
             v1 = chr(ord(max_variables) + 1)
             v2 = chr(ord(max_variables) + 2)
-            variables.extend([v1, v2])
+            _variables.extend([v1, v2])
             n_occurrences.extend([0, 0])
             continue
 
@@ -50,21 +61,21 @@ def generate_random_formula():
 
         # updates the vector of occurrences
         for j in [x1, x2, x3]:
-            n_occurrences[variables.index(j)] += 1
+            n_occurrences[_variables.index(j)] += 1
 
     return _phi[:-3]
 
 
 def create_graph(formula):
-    edges = pd.DataFrame(columns=['u', 'v'])
-    literals_for_each_clauses = []
-    clauses = formula.split(' ^ ')
+    _edges = pd.DataFrame(columns=['u', 'v'])
+    _literals_for_each_clauses = []
+    _clauses = formula.split(' ^ ')
     n_occurrences = []
-    variables = []
+    _variables = []
 
-    for i in range(len(clauses)):
+    for i in range(len(_clauses)):
         # get literals for each clause
-        literals = re.findall('-?\w', clauses[i])
+        literals = re.findall('-?\w', _clauses[i])
         l1, l2, l3 = literals[0], literals[2], literals[4]
 
         # get variables for each clause
@@ -74,16 +85,16 @@ def create_graph(formula):
         # create edge (u, v) where u = xj and v = Ci
         for j in [x1, x2, x3]:
             try:
-                index = variables.index(j)
+                index = _variables.index(j)
                 n_occurrences[index] += 1
             except ValueError:
-                variables.append(j)
+                _variables.append(j)
                 n_occurrences.append(1)
-            edge = {"u": j, "v": i + 1}
-            edges = pd.concat([edges, pd.DataFrame([edge])])
+            _edge = {"u": j, "v": i + 1}
+            _edges = pd.concat([_edges, pd.DataFrame([_edge])])
 
         # append literals array
-        literals_for_each_clauses.append([l1, l2, l3])
+        _literals_for_each_clauses.append([l1, l2, l3])
 
     if min(n_occurrences) != 3:
         print('The formula entered is not a 3sat-3 formula.')
@@ -91,58 +102,67 @@ def create_graph(formula):
         exit()
 
     # generate array of clauses node
-    item = [i for i in range(1, len(clauses))]
+    item = [i for i in range(1, len(_clauses))]
 
     # create and plot graph
-    graph = nx.Graph()
-    graph.add_nodes_from(variables, bipartite=0)
-    graph.add_nodes_from(item, bipartite=2)
-    graph.add_edges_from(tuple(x) for x in edges.values)
+    _graph = nx.Graph()
+    _graph.add_nodes_from(_variables, bipartite=0)
+    _graph.add_nodes_from(item, bipartite=2)
+    _graph.add_edges_from(tuple(x) for x in _edges.values)
 
-    pos = bipartite_layout(graph, variables)
+    pos = bipartite_layout(_graph, _variables)
     plt.figure(figsize=(10, 10))
-    nx.draw_networkx(graph, pos, with_labels=True, node_size=1000, node_color='r', edge_color='g', arrowsize=10)
+    nx.draw_networkx(_graph, pos, with_labels=True, node_size=1000, node_color='r', edge_color='g', arrowsize=10)
     plt.savefig('./bipartite-graph.jpeg')
     plt.close()
 
     # create csr_matrix
-    matrix = np.zeros((len(variables), len(clauses)), dtype=int)
-    for i, edge in edges.iterrows():
-        xi = edge['u']
-        cj = edge['v']
-        matrix[variables.index(xi)][cj - 1] = 1
+    matrix = np.zeros((len(_variables), len(_clauses)), dtype=int)
+    for i, _edge in _edges.iterrows():
+        xi = _edge['u']
+        cj = _edge['v']
+        matrix[_variables.index(xi)][cj - 1] = 1
 
-    return variables, clauses, literals_for_each_clauses, csr_matrix(matrix)
+    return _variables, _clauses, _literals_for_each_clauses, csr_matrix(matrix)
 
 
-def plot_matching(variables, clauses, edges):
+def plot_matching(_variables, _clauses, _edges):
     # generate array of clauses node
-    item = [i for i in range(1, len(clauses))]
+    item = [i for i in range(1, len(_clauses))]
 
     # create and plot graph
-    graph = nx.Graph()
-    graph.add_nodes_from(variables, bipartite=0)
-    graph.add_nodes_from(item, bipartite=2)
-    graph.add_edges_from(tuple(x) for x in edges.values)
+    _graph = nx.Graph()
+    _graph.add_nodes_from(_variables, bipartite=0)
+    _graph.add_nodes_from(item, bipartite=2)
+    _graph.add_edges_from(tuple(_edge) for _edge in _edges.values)
 
-    pos = bipartite_layout(graph, variables)
+    pos = bipartite_layout(_graph, _variables)
     plt.figure(figsize=(10, 10))
-    nx.draw_networkx(graph, pos, with_labels=True, node_size=1000, node_color='r', edge_color='g', arrowsize=10)
+    nx.draw_networkx(_graph, pos, with_labels=True, node_size=1000, node_color='r', edge_color='g', arrowsize=10)
     plt.savefig('./perfect-matching.jpeg')
     plt.close()
 
 
 if __name__ == '__main__':
-    """
-    print('Enter the formula 3sat3 using this format.')
-    print('Use "-" for the "NOT" operation. For example: NOT A is equal to -A')
-    print('Use "v" to separate literals in clause. For example: (A v B v -C).')
-    print('Use "^" to separate clauses from each other. For example: (A v B v -C) ^ (A v E v D)')
-    formula = input('Enter the formula: ')
-    """
-    phi = generate_random_formula()
-    print('This is the generated formula:')
-    print(phi)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-gen', dest='gen', action='store',
+                        default=False, type=str2bool, help='generate phi formula')
+    args = parser.parse_args()
+
+    if args.gen:
+        phi = generate_random_formula()
+        print('This is the generated formula:')
+        print(phi)
+    else:
+        print('Enter the formula 3sat3 using this format.')
+        print('Use "-" for the "NOT" operation. For example: NOT A is equal to -A')
+        print('Use "v" to separate literals in clause. For example: (A v B v -C).')
+        print('Use "^" to separate clauses from each other. For example: (A v B v -C) ^ (A v E v D)')
+        phi = input('Enter the formula: ')
+        if not re.fullmatch(PHI_REGEX, phi):
+            print('The input entered is not a valid formula.')
+            exit()
 
     # create graph
     variables, clauses, literals_for_each_clauses, graph = create_graph(phi)
@@ -158,7 +178,6 @@ if __name__ == '__main__':
         u = variables[matching[i]]
         v = i + 1
         edge = {"u": u, "v": v}
-        print(edge)
         edges = pd.concat([edges, pd.DataFrame([edge])])
 
         # if u appears in the v negated clause then u = 0, otherwise 1
@@ -169,7 +188,6 @@ if __name__ == '__main__':
     # plot matching
     plot_matching(variables, clauses, edges)
 
-    # print assignment
     print('The assignment that satisfies the formula is as follows:')
     for i in range(len(variables)):
         print(f'{variables[i]}: {"False" if output[i] == 0 else "True"}')
